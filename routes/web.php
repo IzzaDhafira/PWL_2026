a43910c7eae3f6687c3775fa968ddabe71199fce
Fira <izzadhafirafanani@gmail.com>
2026-02-23 13:28:11 +0700

    Praktikum1_J2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+   return 'World';
+});
+
+Route::get('/selamatdatang', function () {
+   return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+   return '244107020106 Izza Dhafira Fanani';
+});
+
+Route::get('/user/{fira}', function ($fira) {
+    return 'Nama saya '.$fira;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}/pages/{page}', function ($articleId, $pageId) {
+    return 'Halaman Artikel ke-'.$pageId." dengan ID: ".$articleId;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+return 'Nama saya '.$name;
 });

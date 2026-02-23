4fc8dce7b924bed61e8fe96836c41d89a8d9f5b4
Fira <izzadhafirafanani@gmail.com>
2026-02-23 13:56:33 +0700

    Praktikum1_J2

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Route::get('/world', function () {
    return 'World';
 });
 
-Route::get('/selamatdatang', function () {
+Route::get('/', function () {
    return 'Selamat Datang';
 });
 
 Route::get('/about', function () {
-   return '244107020106 Izza Dhafira Fanani';
+   return 'NIM: 244107020106 <br> Nama: Izza Dhafira Fanani';
 });
 
 Route::get('/user/{fira}', function ($fira) {
@@ -37,8 +37,8 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
 
-Route::get('/articles/{id}/pages/{page}', function ($articleId, $pageId) {
-    return 'Halaman Artikel ke-'.$pageId." dengan ID: ".$articleId;
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
 });
 
 Route::get('/user/{name?}', function ($name=null) {

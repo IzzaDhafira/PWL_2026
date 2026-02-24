e2317a5730844f375da8f10e116e44a7528202a1
Fira <izzadhafirafanani@gmail.com>
2026-02-24 09:19:18 +0700

    Praktikum2_J2

--- routes/web.php
+++ routes/web.php
@@ -57,5 +57,12 @@ Route::get('/articles/{id}', ArticleController::class);
 
 Route::resource('photos', PhotoController::class);
 
+Route::resource('photos', PhotoController::class)->only([
+ 'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+ 'create', 'store', 'update', 'destroy'
+]);
+
 Route::get('/greeting', [WelcomeController::class, 'greeting']);
 
